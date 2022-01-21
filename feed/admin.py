738008793930f24d8284aa9514712b3fdda55248
chanvandaneb <ahyev42@gmail.com>
2022-01-21 22:06:43 +0700

    Image Sharer Open Source Django

---
+++ feed/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+
+
+    
---
+++ feed/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class PostForm(forms.Form):
+    image = forms.ImageField()
+    text = forms.CharField(label="Description")
